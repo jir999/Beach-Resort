26eef27242148bca85abb45297e35dd2516f516a
jir999 <jir.avetisyan@gmail.com>
2020-03-03 22:54:38 +0400

    First commit

---
+++ src/pages/Rooms.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Room(){
+    return(
+        <div>
+        
+        </div>
+    )
+}
+export default Room;
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class SingleRoom extends React.Component{
+    render(){
+        return(
+            <div>
+            
+            </div>
+        )
+    }
+}
+export default SingleRoom;
