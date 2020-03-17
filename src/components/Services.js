a55308db8b1f30746bfea1ced60f9031245a9978
jir999 <jir.avetisyan@gmail.com>
2020-03-17 23:33:40 +0400

    Home , Services

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,12 +1,52 @@
 import React, {Component} from 'react';
+import {FaCocktail, FaHiking, FaShuttleVan, FaBeer} from "react-icons/fa";
 import Title from "./Title";
 
 export default class Services extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            services: [
+                {
+                    icon: <FaCocktail />,
+                    title: "free cocktails",
+                    info: 'lorem ipsum...'
+                },
+                {
+                    icon: <FaHiking />,
+                    title: "Endless Hiking",
+                    info: 'lorem ipsum...'
+                },
+                {
+                    icon: <FaShuttleVan />,
+                    title: "Free shuttle",
+                    info: 'lorem ipsum...'
+                },
+                {
+                    icon: <FaBeer />,
+                    title: "Strongest Beer",
+                    info: 'lorem ipsum...'
+                }
+            ]
+        }
+    }
+
     render(){
         return(
-            <div>
+            <section className="services">
                 <Title title="services" />
-            </div>
+                <div className="services-center">
+                    {this.state.services.map((item,index)=>{
+                        return (
+                            <article key={index} className="service">
+                                <span>{item.icon}</span>
+                                <h6>{item.title}</h6>
+                                <p>{item.info}</p>
+                            </article>
+                        )
+                    })}
+                </div>
+            </section>
         )
     }
 }
